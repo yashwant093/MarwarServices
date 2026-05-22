d2b75d1fa444899aad59b99c2999add557a40107
Yashwant Lohar <malviyayash97@gmail.com>
2026-05-22 22:49:13 +0530

    Disable source maps for production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    sourcemap: false,
+
+    minify: 'esbuild',
+  },
 })
